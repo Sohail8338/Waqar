d89ce2d9ee78ba046725fac868e485dc67830abf
Sohail Khan Shinwari <sohail8338@gmail.com>
2015-12-08 14:50:28 +0500

    Update w.cpp

--- w.cpp
+++ w.cpp
@@ -1,4 +1,7 @@
-nclude<stdio.h>
+#include<stdio.h>
+ #include <iostream>
+ using namespace std ;
+ int multiply(int , int) ;
  
  int main()
  {
@@ -6,11 +9,15 @@ nclude<stdio.h>
      
         printf("Enter two numbers to add\n");
 	   scanf("%d%d",&a,&b);
-	    
-	       c = a + b;
-	        
+	   cout << multiply(a, b) ;
+	       c = a + b ;
 		   printf("Sum of entered numbers = %d\n",c);
 		    
 		       return 0;
-		       }
+}
+
+int multiply(int a , int b )
+{
+	return a*b ;
+}
 
